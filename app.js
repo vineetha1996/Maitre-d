b63f823f83f9ca557c9d9a3a069a3d3847c0da37
Vineetha Yenugula <vineethay96@gmail.com>
2020-02-16 14:47:09 -0600

    .

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ function app()
      const Rate=parseFloat(document.querySelector('#rate-field').value)
 
      console.log(Rate);
+     
      const Tip=Total*(Rate/100)
      const TaxRate=5.5
      const Tax=Total*(TaxRate/100)
      const GrandTotal=Total+Tip+Tax;
-     
+
      document.querySelector('#Tip').innerHTML=`$${Tip.toFixed(2)}`
      document.querySelector('#Tax').innerHTML=`$${Tax.toFixed(2)}`
      document.querySelector('#Total').innerHTML=`$${GrandTotal.toFixed(2)}`
